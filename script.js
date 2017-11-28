681ead5bfa8ee8f4fc79f5530db54972e247a630
Adin Gilman-Cohen <adin1541@Adins-MacBook-Pro.local>
2017-11-27 17:49:49 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculateCurrentGrade(){
+    var inputTests= document.getElementById("tests").value
+    var arrayTests= string.split(“inputTests”);
+}
+
+function convertArrayStringToNumber(){
+
+}
+
+function averageArray(array){
+
+    for (var i=0; i<arrayTests.length; i++){
+        array[i] = parseInt(array[i])
+
+    }
+}
+
+function calculateGradeNeeded(){
+
+}
